b6b7af76023f60f24339100cd10b193a3faaef3f
VTStanchev18 <VTStanchev18@codingburgas.bg>
2020-08-26 14:22:14 +0300

    Add alphabetSoup

---
+++ alphabetSoup/alphabetSoup.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+string alphabetSoup(string str)
+{
+	char a;
+
+	for (size_t i = 0; i < str.length() - 1; i++)
+	{
+		for (size_t j = 0; j < str.length() - i - 1; j++)
+		{
+			if (str[j] > str[j + 1])
+			{
+				a = str[j];
+				str[j] = str[j + 1];
+				str[j + 1] = a;
+			}
+		}
+	}
+
+	return str;
+}
+
+int main()
+{
+	cout << alphabetSoup("edabit");
+}
+
+
