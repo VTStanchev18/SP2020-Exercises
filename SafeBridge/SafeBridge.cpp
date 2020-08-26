17b60ad51d063b400719ecbf7a51bfd832f19a63
VTStanchev18 <VTStanchev18@codingburgas.bg>
2020-08-26 12:07:01 +0300

    Add isSafeBridge

---
+++ SafeBridge/SafeBridge.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isSafeBridge(string str) {
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	cout << isSafeBridge("#######");
+}
+
